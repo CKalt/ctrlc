fb7180fcc5fbb3230d0c04a83d4a3518c7c6333d
Chris Kaltwasser <chris@chrisk.com>
2021-12-12 18:05:34 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io::prelude::*;
+use std::net::{TcpStream, Shutdown};
+use std::sync::Arc;
+
+fn main() {
+    let stream = TcpStream::connect("127.0.0.1:5555").unwrap();
+    let stream = Arc::new(stream);
+    let handle = Arc::clone(&stream);
+    ctrlc::set_handler(move || {
+        let _ = handle.shutdown(Shutdown::Read);
+    }).unwrap();
+    loop {
+        let mut buf = [0; 100];
+        if let Err(_) = (&*stream).read_exact(&mut buf) {
+            eprintln!("got an error, exiting...");
+            break;
+        }
+        eprintln!("got some bytes");
+    }
+}
