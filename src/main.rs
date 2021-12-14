e5bb4c57b7856fe97580018f113bee41a50c8f6f
Chris Kaltwasser <chris@chrisk.com>
2021-12-14 09:53:50 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
 use std::io::prelude::*;
 use std::net::{TcpStream, Shutdown};
 use std::sync::Arc;
-//use std::sync::atomic::{AtomicBool, Ordering};
-use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "example", about = "An example of StructOpt usage.")]
-struct Opt {
-    #[structopt(short, long)]
-    use_arc: bool,
-}
-
-fn do_arc_trial() {
+fn main() {
     let stream = TcpStream::connect("127.0.0.1:5555").unwrap();
     let stream = Arc::new(stream);
     let handle = Arc::clone(&stream);
@@ -26,15 +17,5 @@ fn do_arc_trial() {
             break;
         }
         eprintln!("got some bytes");
-    }
-}
-
-fn main() {
-    let opt = Opt::from_args();
-
-    if opt.use_arc {
-        do_arc_trial();
-    } else {
-        println!("atomic bool usage");
     }
 }
