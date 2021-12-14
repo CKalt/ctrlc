74a536ac61871b1558485f9dbb4aa573d1c466b6
Chris Kaltwasser <chris@chrisk.com>
2021-12-14 11:46:50 -0500

    mult sessions

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,60 @@
 use std::io::prelude::*;
 use std::net::{TcpStream, Shutdown};
-use std::sync::Arc;
+use std::sync::{Mutex, Arc};
+use std::{thread, time};
+
+struct Connection {
+    opt_stream: Option<TcpStream>,
+}
 
 fn main() {
-    let stream = TcpStream::connect("127.0.0.1:5555").unwrap();
-    let stream = Arc::new(stream);
-    let handle = Arc::clone(&stream);
+    let mt_stream = Arc::new(Mutex::new(
+                    Connection {
+                        opt_stream: None
+                    }));
+
+    let handle = Arc::clone(&mt_stream);
     ctrlc::set_handler(move || {
         println!("Ctrl-C was received...");
-        let _ = handle.shutdown(Shutdown::Read);
+        let safe_stream = handle.lock().unwrap();
+        safe_stream.opt_stream.as_ref().unwrap().shutdown(Shutdown::Read).unwrap();
     }).unwrap();
+
+    let mut session_num = 1;
     loop {
-        let mut buf = [0; 100];
-        if let Err(_) = (&*stream).read_exact(&mut buf) {
-            eprintln!("got an error, exiting...");
+        {
+            let mut safe_stream = mt_stream.lock().unwrap();
+            if let None = safe_stream.opt_stream {
+                safe_stream.opt_stream =
+                    Some(TcpStream::connect("127.0.0.1:5555").unwrap());
+            } else {
+                panic!("expected disconnected stream");
+            }
+        }
+
+        loop {
+            let mut buf = [0; 100];
+            {
+                let safe_stream = mt_stream.lock().unwrap();
+
+                if let Err(_) =
+                        safe_stream.opt_stream.as_ref().unwrap().read_exact(&mut buf) {
+                    println!("got an error, exiting...");
+                    break;
+                }
+            }
+            println!("Seesion Number {}, got some bytes", session_num);
+        }
+
+        {
+            let mut safe_stream = mt_stream.lock().unwrap();
+            safe_stream.opt_stream = None; // disconnect
+        }
+        thread::sleep(time::Duration::from_secs(1));
+
+        if session_num >= 5 {
             break;
         }
-        eprintln!("got some bytes");
+        session_num += 1;
     }
 }
