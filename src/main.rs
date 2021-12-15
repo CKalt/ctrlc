bc99fb3ce149e5149e2037bf8db7899b8287a4c8
Chris Kaltwasser <chris@chrisk.com>
2021-12-15 11:54:47 -0500

    changed Connection to a simple type definition.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 use std::io::prelude::*;
 use std::net::{TcpStream, Shutdown};
 use std::sync::{Mutex, Arc};
+use std::ops::DerefMut;
 use std::{thread, time};
 
-struct Connection {
-    opt_stream: Option<TcpStream>,
-}
+type Connection = Option<TcpStream>;
 
 fn main() {
-    let mt_stream = Arc::new(Mutex::new(
-                    Connection {
-                        opt_stream: None
-                    }));
+    let mt_stream: Arc<Mutex<Connection>> = Arc::new(Mutex::new(None));
 
     let handle = Arc::clone(&mt_stream);
     ctrlc::set_handler(move || {
         println!("Ctrl-C was received...");
         let safe_stream = handle.lock().unwrap();
-        safe_stream.opt_stream.as_ref().unwrap().shutdown(Shutdown::Read).unwrap();
+        safe_stream.as_ref().unwrap().shutdown(Shutdown::Read).unwrap();
     }).unwrap();
 
     let mut session_num = 1;
     loop {
         {
-            let mut safe_stream = mt_stream.lock().unwrap();
-            if let None = safe_stream.opt_stream {
-                safe_stream.opt_stream =
+            let mut safe_stream_mutex = mt_stream.lock().unwrap();
+            let safe_stream = safe_stream_mutex.deref_mut();
+            if let None = safe_stream {
+                *safe_stream =
                     Some(TcpStream::connect("127.0.0.1:5555").unwrap());
             } else {
                 panic!("expected disconnected stream");
@@ -38,7 +35,7 @@ fn main() {
                 let safe_stream = mt_stream.lock().unwrap();
 
                 if let Err(_) =
-                        safe_stream.opt_stream.as_ref().unwrap().read_exact(&mut buf) {
+                        safe_stream.as_ref().unwrap().read_exact(&mut buf) {
                     println!("got an error, exiting...");
                     break;
                 }
@@ -47,8 +44,9 @@ fn main() {
         }
 
         {
-            let mut safe_stream = mt_stream.lock().unwrap();
-            safe_stream.opt_stream = None; // disconnect
+            let mut safe_stream_mutex = mt_stream.lock().unwrap();
+            let safe_stream = safe_stream_mutex.deref_mut();
+            *safe_stream = None; // disconnect
         }
         thread::sleep(time::Duration::from_secs(1));
 
