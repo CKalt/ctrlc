ff5625c7608b5c692804d94b12fdf97dcb08a558
Chris Kaltwasser <chris@chrisk.com>
2021-12-17 10:31:58 -0500

    changed from mutex to rwlock

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use std::io::prelude::*;
 use std::net::{TcpStream, Shutdown};
-use std::sync::{Mutex, Arc};
+use std::sync::{RwLock, Arc};
 use std::ops::DerefMut;
 use std::{thread, time};
 
 type Connection = Option<TcpStream>;
 
 fn main() {
-    let mt_stream: Arc<Mutex<Connection>> = Arc::new(Mutex::new(None));
+    let mt_stream: Arc<RwLock<Connection>> = Arc::new(RwLock::new(None));
 
     let handle = Arc::clone(&mt_stream);
     ctrlc::set_handler(move || {
         println!("Ctrl-C was received...");
-        let safe_stream = handle.lock().unwrap();
+        let safe_stream = handle.read().unwrap();
         safe_stream.as_ref().unwrap().shutdown(Shutdown::Read).unwrap();
     }).unwrap();
 
     let mut session_num = 1;
     loop {
         {
-            let mut safe_stream_mutex = mt_stream.lock().unwrap();
-            let safe_stream = safe_stream_mutex.deref_mut();
-            if let None = safe_stream {
+            let mut safe_stream_rwlock = mt_stream.write().unwrap();
+            let safe_stream = safe_stream_rwlock.deref_mut();
+            if safe_stream.is_none() {
                 *safe_stream =
                     Some(TcpStream::connect("127.0.0.1:5555").unwrap());
             } else {
@@ -32,7 +32,7 @@ fn main() {
         loop {
             let mut buf = [0; 100];
             {
-                let safe_stream = mt_stream.lock().unwrap();
+                let safe_stream = mt_stream.read().unwrap();
 
                 if let Err(_) =
                         safe_stream.as_ref().unwrap().read_exact(&mut buf) {
@@ -44,8 +44,8 @@ fn main() {
         }
 
         {
-            let mut safe_stream_mutex = mt_stream.lock().unwrap();
-            let safe_stream = safe_stream_mutex.deref_mut();
+            let mut safe_stream_rwlock = mt_stream.write().unwrap();
+            let safe_stream = safe_stream_rwlock.deref_mut();
             *safe_stream = None; // disconnect
         }
         thread::sleep(time::Duration::from_secs(1));
